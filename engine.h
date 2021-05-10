b7fb00c955a02a5d9b413e92c9d823be20abc7eb
xorspark <xorspark@sparkant.com>
2021-05-10 00:39:24 -0400

    Include guards are a thing.

--- engine.h
+++ engine.h
@@ -1,2 +1,6 @@
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
 void initBuffer();
 void blitFire(unsigned int *display);
+#endif
--- util.h
+++ util.h
@@ -1,1 +1,5 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
 double genrand();
+#endif
